58d867b70eb9d5ff92fb8ab0000dccfb940686b7
aevtikheev <eft000@gmail.com>
2021-01-09 02:17:45 +0300

    Add basic handlers for telegram bot

--- questions.py
+++ questions.py
@@ -9,4 +9,22 @@ def get_random_question():
     """TODO"""
     with open(env_settings.questions_file) as questions_file:
         questions = json.load(questions_file)
-        return random.choice(list(questions.items()))
+        return random.choice(list(questions.keys()))
+
+
+def get_answer(question):
+    """TODO"""
+    with open(env_settings.questions_file) as questions_file:
+        questions = json.load(questions_file)
+        return questions[question]
+
+
+def is_correct_answer(user_answer, true_answer):
+    """TODO"""
+    exact_answer = true_answer
+    if '(' in true_answer:
+        exact_answer = true_answer.split('(')[0]
+    if '.' in true_answer:
+        exact_answer = true_answer.split('.')[0]
+    exact_answer = exact_answer.rstrip()
+    return user_answer == exact_answer
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,37 +1,125 @@
 """Telegram version of a support bot."""
+from enum import Enum
+
 from telegram import Update, ReplyKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          CallbackContext, ConversationHandler, RegexHandler)
+from redis import Redis
 
 from env_settings import env_settings
-from questions import get_random_question
-from redis_adapter import redis_adapter
+from questions import get_random_question, get_answer, is_correct_answer
 
 
-NEW_QUESTION = 'Новый вопрос'
-GIVE_UP = 'Сдаться'
-SCORE = 'Мой счёт'
+NEW_QUESTION_TEXT = 'Новый вопрос'
+GIVE_UP_TEXT = 'Сдаться'
+SCORE_TEXT = 'Мой счёт'
+
+BOT_DATA_REDIS_DB = 'redis_db'
+
+menu_buttons = [[NEW_QUESTION_TEXT, GIVE_UP_TEXT], [SCORE_TEXT]]
+reply_markup = ReplyKeyboardMarkup(menu_buttons)
 
 
-def echo_handler(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    custom_keyboard = [[NEW_QUESTION, GIVE_UP], [SCORE]]
-    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+class BotState(Enum):
+    MENU = 'Choice'
+    QUESTION = 'Question'
 
-    message = update.message.text
-    if message == NEW_QUESTION:
-        question, answer = get_random_question()
-        update.message.reply_text(question, reply_markup=reply_markup)
+
+def handle_start(update: Update, context: CallbackContext):
+    """TODO"""
+    update.message.reply_text(
+        f'Добрый день! Нажмите "{NEW_QUESTION_TEXT}" для начала игры.',
+        reply_markup=reply_markup
+    )
+
+    return BotState.MENU
+
+
+def handle_new_question_request(
+        update: Update,
+        context: CallbackContext
+):
+    """TODO"""
+    redis_db = context.bot_data[BOT_DATA_REDIS_DB]
+    question = get_random_question()
+    redis_db.set(update.effective_user.id, question)
+    update.message.reply_text(question)
+
+    return BotState.QUESTION
+
+
+def handle_solution_attempt(
+        update: Update,
+        context: CallbackContext
+):
+    """TODO"""
+    redis_db = context.bot_data[BOT_DATA_REDIS_DB]
+    question = redis_db.get(update.effective_user.id).decode('utf-8')
+    answer = get_answer(question)
+    if is_correct_answer(update.message.text, answer):
+        reply_text = 'Поздравляем! Ответ верен. Ещё разок?'
     else:
-        update.message.reply_text(update.message.text, reply_markup=reply_markup)
+        reply_text = f'Неправильно :( Правильный ответ - "{answer}". Хотите попробовать ещё раз?'
+    update.message.reply_text(reply_text)
+
+    return BotState.MENU
+
+
+def handle_give_up_request(
+        update: Update,
+        context: CallbackContext
+):
+    """TODO"""
+    redis_db = context.bot_data[BOT_DATA_REDIS_DB]
+    question = redis_db.get(update.effective_user.id).decode('utf-8')
+    answer = get_answer(question)
+    update.message.reply_text(f'Правильный ответ: "{answer}". Хотите попробовать ещё раз?')
+
+    return BotState.MENU
 
 
 def start_bot() -> None:
     """Start Telegram bot."""
+    redis_db = Redis(
+            host=env_settings.redis_host,
+            port=env_settings.redis_port,
+            password=env_settings.redis_password
+        )
+
     bot_token = env_settings.tg_bot_token
     updater = Updater(bot_token)
     dispatcher = updater.dispatcher
+    dispatcher.bot_data[BOT_DATA_REDIS_DB] = redis_db
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo_handler, pass_user_data=True))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', handle_start)],
+
+        states={
+            BotState.MENU: [
+                MessageHandler(
+                    Filters.regex(NEW_QUESTION_TEXT),
+                    handle_new_question_request,
+                    pass_user_data=True
+                )
+            ],
+
+            BotState.QUESTION: [
+                MessageHandler(
+                    Filters.regex(GIVE_UP_TEXT),
+                    handle_give_up_request,
+                    pass_user_data=True
+                ),
+                MessageHandler(
+                    Filters.text & ~Filters.command,
+                    handle_solution_attempt,
+                    pass_user_data=True
+                )
+            ],
+        },
+
+        fallbacks=[ConversationHandler.END]
+    )
+    dispatcher.add_handler(conv_handler)
 
     updater.start_polling()
     updater.idle()
