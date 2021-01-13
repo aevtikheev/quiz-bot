0070c47ab096562b50394645ecaf2f26ab5a34fb
aevtikheev <eft000@gmail.com>
2021-01-13 15:09:49 +0300

    Add raw questions parser, extend questions.json

--- questions.py
+++ questions.py
@@ -1,9 +1,20 @@
 """Common functions to work with questions."""
+import pathlib
 import json
 import random
+import logging
+from typing import Tuple
 
 
 CHEAT_PHRASE = 'cheat'
+PARSE_QUESTIONS_FILE_TIMEOUT = 5
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger()
 
 
 class QuizDB:
@@ -30,3 +41,45 @@ def is_correct_answer(user_answer: str, true_answer: str, cheating=False) -> boo
         return True
 
     return user_answer.lower() == true_answer.lower()
+
+
+def _parse_block(block: str) -> Tuple[str, str]:
+    """Extract question and answer from a text block."""
+    question_start = block.find('\n', block.find('Вопрос')) + 1
+    question_end = block.find('\n\n', question_start)
+    question = block[question_start:question_end]
+    question = " ".join(question.split())
+
+    answer_start = block.find('\n', block.find('Ответ')) + 1
+
+    end_symbols_positions = [position for position in (
+        block.find('.', answer_start),
+        block.find('(', answer_start),
+        block.find('\n', answer_start)
+    ) if position != -1]  # position is -1 if the symbol is not in the block.
+
+    answer_end = min(end_symbols_positions) if end_symbols_positions else -1
+    answer = block[answer_start:answer_end].strip()
+
+    return question, answer
+
+
+def parse_questions(raw_data_folder: str, parsed_questions_file: str) -> None:
+    """Parse raw questions data and write parsed data to a file."""
+    raw_data_folder_content = pathlib.Path(raw_data_folder).glob('**/*')
+    question_files_list = [item for item in raw_data_folder_content if item.is_file()]
+
+    questions = dict()
+
+    for questions_file_name in question_files_list:
+        with open(questions_file_name, 'r', encoding='KOI8-R') as questions_file:
+            questions_file_blocks = questions_file.read().split('\n\n\n')
+            for block in questions_file_blocks:
+                if 'Вопрос' in block and 'Ответ' in block:
+                    question, answer = _parse_block(block)
+
+                questions[question] = answer
+            logger.info(f'File {questions_file_name} parsed')
+
+    with open(parsed_questions_file, 'w') as output_file:
+        output_file.write(json.dumps(questions, ensure_ascii=False, indent=2))
--- run.py
+++ run.py
@@ -4,10 +4,11 @@ import sys
 
 import telegram_bot
 import vk_bot
-
+from questions import parse_questions
 
 CMD_TELEGRAM_BOT = 'telegram_bot'
 CMD_VK_BOT = 'vk_bot'
+CMD_PARSE_QUESTIONS = 'parse_questions'
 
 
 def parse_args() -> argparse.Namespace:
@@ -16,6 +17,18 @@ def parse_args() -> argparse.Namespace:
 
     subparsers.add_parser(CMD_TELEGRAM_BOT, help='Run telegram bot')
     subparsers.add_parser(CMD_VK_BOT, help='Run VK bot')
+
+    questions_parser = subparsers.add_parser(CMD_PARSE_QUESTIONS, help='Parse raw questions data')
+    questions_parser.add_argument(
+        '-q', '--questions_folder', type=str,
+        help='Folder with question\'s raw data',
+        required=True
+    )
+    questions_parser.add_argument(
+        '-f', '--output_file', type=str,
+        help='File where parsed questions will be stored',
+        required=True
+    )
 
     if len(sys.argv) <= 1:
         sys.argv.append('--help')
@@ -29,6 +42,8 @@ def main() -> None:
         telegram_bot.start_bot()
     elif args.command == CMD_VK_BOT:
         vk_bot.start_bot()
+    elif args.command == CMD_PARSE_QUESTIONS:
+        parse_questions(args.questions_folder, args.output_file)
 
 
 if __name__ == '__main__':
